4c8f348be267560235a02679efd8e2a7f96d1d2c
MirekSh <miroslaw@shedul.com>
2020-04-26 21:19:20 +0200

    Add isSlotAvailable prototype

--- public/script.js
+++ public/script.js
@@ -7,19 +7,48 @@ const SHIPS = [
 ]
 
 const fleet1 = document.querySelector('.fleet-1');
-const fleet2 = document.querySelector('.fleet-2');
-const fields = document.querySelectorAll('td');
+const yourFleet = document.querySelector('.yourFleet')
+const cells = document.querySelectorAll('.cell');
+
+console.log("yourFleet", yourFleet)
 
 const fleet = SHIPS.reduce((fl, ship) => fl += `<p class="name"><span>${ship.name}</span> x ${ship.number}</p>
         <p class="size">Size: ${ship.size} <img src="./media/${ship.name.toLowerCase()}.png" alt="${ship.name}-img" /> </p>`, ''
 )
 
 fleet1.insertAdjacentHTML('afterbegin', fleet);
-fleet2.insertAdjacentHTML('afterbegin', fleet);
 
 function addShip() {
-    this.classList.toggle('fa');
     this.classList.toggle('fa-check');
 }
 
-fields.forEach(field => field.addEventListener('click', addShip));
+cells.forEach(cell => cell.addEventListener('click', addShip));
+
+
+function isSlotAvailable(shipLength) {
+    const possibilities = [];
+    cells.forEach(cell => {
+        let { row, column } = cell.dataset;
+        row = Number(row);
+        column = Number(column);
+        if (row + shipLength <= 9) {
+            const slot = [];
+            for (let i = row; i < row + shipLength; i++) {
+                if (!cell.classList.contains('fa-check')) slot.push([i, column]);
+            }
+            possibilities.push(slot);
+        }
+        if (column + shipLength <= 9) {
+            const slot = [];
+            for (let i = column; i < column + shipLength; i++) {
+                if (!cell.classList.contains('fa-check')) slot.push([row, i]);
+            }
+            possibilities.push(slot);
+        }
+    })
+    const randomSlot = possibilities[Math.floor(Math.random() * possibilities.length)];
+    randomSlot.forEach(coords => [...cells].find(cell => cell.dataset.row == coords[0] && cell.dataset.column == coords[1]).classList.toggle('fa-check'))
+}
+
+isSlotAvailable(5);
+isSlotAvailable(4);
