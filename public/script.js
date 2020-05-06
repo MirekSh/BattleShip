c5c50302f4ac6fc0e9ca43f0ddff46f5a4d875be
MirekSh <miroslaw@shedul.com>
2020-05-06 23:54:42 +0200

    Refacor js code; Add round counter

--- public/script.js
+++ public/script.js
@@ -10,31 +10,25 @@ const fleet1 = document.querySelector('.fleet-1');
 const yourFleet = document.querySelector('.yourFleet')
 const cells = document.querySelectorAll('.yourFleet .cell');
 const boardCells = document.querySelectorAll('.playingBoard .cell');
+const playingBoard = document.querySelector('.playingBoard');
 const signUp = document.querySelector('.signUp');
 const logIn = document.querySelector('.logIn');
-const close = document.querySelectorAll('.close');
+const closeBtn = document.querySelectorAll('.close');
+const singUpBtn = document.querySelector('.signUpBtn');
+const logoutBtn = document.querySelector('.logout');
+const loginBtn = document.querySelector('.loginBtn');
+const counter = document.querySelector('.counter');
 const coordsTable = [];
 let opponentBoard;
 let fieldsArray = [];
+let game = {};
+let otherPlayer;
+let yourRounds = 0;
 
-close.forEach(btn => btn.addEventListener('click', () => {
-    document.querySelector('.signPopup').style.display = 'none';
-    document.querySelector('.logInPopup').style.display = 'none';
-}))
-
-signUp.addEventListener('click', () => {
-    document.querySelector('.signPopup').style.display = 'flex';
-})
-
-logIn.addEventListener('click', () => {
-    document.querySelector('.logInPopup').style.display = 'flex';
-})
 
 const fleet = SHIPS.reduce((fl, ship) => fl += `<p class="name"><span>${ship.name}</span> x ${ship.number}</p>
         <p class="size">Size: ${ship.size} <img src="./media/${ship.name.toLowerCase()}.png" alt="${ship.name}-img" /> </p>`, ''
 )
-
-fleet1.insertAdjacentHTML('afterbegin', fleet);
 
 function addShip() {
     this.classList.toggle('fa-check');
@@ -42,7 +36,7 @@ function addShip() {
 
 function setupBoard(data, email) {
     const opponentBoard = data.map(el => el.data()).filter(el => el.user_email != email).reverse()[0];
-    console.log("setupBoard -> opponentBoard", opponentBoard.board)
+
     for(ship in opponentBoard.board) {
         for(field in opponentBoard.board[ship]) {
             const { x, y } = opponentBoard.board[ship][field];
@@ -52,22 +46,28 @@ function setupBoard(data, email) {
 }
 
 function checkMarkedCell() {
-    let i;
-    const checked = fieldsArray.find((el, index) => {
-        i = index;
-        return this.dataset.row == el[0] && this.dataset.column == el[1]
-    });
-    if (checked) {
-        this.classList.toggle('fa-check');
-        fieldsArray.splice(i, 1);
-        console.log("checkMarkedCell -> fieldsArray", fieldsArray)
-        if (fieldsArray.length === 0) alert('There is no more ships');
-    } else {
-        this.classList.toggle('fa-times-circle-o');
+    console.log("checkMarkedCell -> yourRounds", yourRounds)
+    if(yourRounds > 0) {
+        let i;
+        const checked = fieldsArray.find((el, index) => {
+            i = index;
+            return this.dataset.row == el[0] && this.dataset.column == el[1]
+        });
+        if (checked) {
+            this.classList.toggle('fa-check');
+            fieldsArray.splice(i, 1);
+            if (fieldsArray.length === 0) alert('There is no more ships');
+        } else {
+            this.classList.toggle('fa-times-circle-o');
+        }
+        yourRounds--;
+        counter.innerText = `You have ${yourRounds} turns`;
+    }
+    else {
+        playingBoard.style.pointerEvents = 'none';
+        console.log("checkMarkedCell -> playingBoard", playingBoard)
     }
 }
-
-boardCells.forEach(boardCell => boardCell.addEventListener('click', checkMarkedCell));
 
 function fillAvailableSlot(shipLength) {
     const possibilities = [];
@@ -110,3 +110,133 @@ function setData() {
     }, {});
 }
 
+function getCounter(user) {
+    return db.collection('rounds').get().then(response => {
+        const round = response.docs.filter(round => round.data().player1.mail == user.email || round.data().player2.mail == user.email).map(round => round.data())[0];
+        console.log("getCounter -> round", round)
+        const playerRound = round.player1.mail == user.email ? round.player1.round : round.player2.round;
+        yourRounds = playerRound;
+        counter.innerText = `You have ${playerRound} turns`;
+    })
+}
+
+function resetCounter(user) {
+    return db.collection('rounds').add({
+        player1: {
+            mail: user.email,
+            round: random * 5,
+        },
+        player2: {
+            mail: otherPlayer.user_email,
+            round: (1 - random) * 5,
+        }
+    })
+}
+
+function remmoveDataFromFirestore(id, collection) {
+    db.collection(collection).doc(id).delete()
+}
+
+function clearData(user) {
+    return db.collection('boards').get().then(response => {
+        const boards = response.docs.filter(doc => doc.data().user_email === user.email);
+        return Promise.all(boards.map(doc =>
+            remmoveDataFromFirestore(doc.id, 'boards')
+        ));
+    }).then(() => db.collection('rounds').get().then(response => {
+        const rounds = response.docs.filter(round => round.data().player1.mail == user.email || round.data().player2.mail);
+        return Promise.all(rounds.map(round =>
+            remmoveDataFromFirestore(round.id, 'rounds')
+        ))
+    }))
+}
+
+function updateBoard(user) {
+        fillAvailableSlot(5);
+        fillAvailableSlot(4);
+        fillAvailableSlot(3);
+        fillAvailableSlot(2);
+        fillAvailableSlot(2);
+
+        return db.collection('boards').add({ user_email: user.email, board: setData() }).then(response => {
+            game.id = response.id;
+            console.log(`Data for ${user.email} added`);
+        }).then(() =>
+            db.collection('boards').get().then(response => {
+                otherPlayer = response.docs.map(el => el.data()).filter(el => el.user_email != user.email)[0];
+                setupBoard(response.docs, user.email);
+            })).then(() => {
+            const random = Math.round(Math.random());
+            return db.collection('rounds').add({
+                player1: {
+                    mail: user.email,
+                    round: random * 5,
+                },
+                player2: {
+                    mail: otherPlayer.user_email,
+                    round: (1 - random) * 5,
+                }
+            })
+        })
+}
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        clearData(user).then(() => updateBoard(user)).then(() => getCounter(user));
+    }
+    else {
+        db.collection('boards').doc(game.id).delete().then(function() {
+            console.log(`Document ${game.id} successfully deleted!`);
+        })
+    }
+})
+
+singUpBtn.addEventListener('click', e => {
+    e.preventDefault();
+
+    const suemail = document.querySelector('.suEmail').value;
+    const supasswd = document.querySelector('.suPassword').value;
+
+    auth.createUserWithEmailAndPassword(suemail, supasswd).then(cred => {
+        console.log(cred);
+    })
+    document.querySelector('.signPopup').style.display = 'none';
+})
+
+logoutBtn.addEventListener('click', e => {
+    e.preventDefault();
+
+    auth.signOut().then(() => {
+        console.log('User logged out');
+    });
+})
+
+loginBtn.addEventListener('click', e => {
+    e.preventDefault();
+
+    const liEmail = document.querySelector('.liEmail').value;
+    const liPasswd = document.querySelector('.liPassword').value;
+
+    auth.signInWithEmailAndPassword(liEmail, liPasswd).then(cred => {
+        console.log(cred);
+    })
+    document.querySelector('.logInPopup').style.display = 'none';
+});
+
+
+closeBtn.forEach(btn => btn.addEventListener('click', () => {
+    document.querySelector('.signPopup').style.display = 'none';
+    document.querySelector('.logInPopup').style.display = 'none';
+}))
+
+signUp.addEventListener('click', () => {
+    document.querySelector('.signPopup').style.display = 'flex';
+})
+
+logIn.addEventListener('click', () => {
+    document.querySelector('.logInPopup').style.display = 'flex';
+})
+
+fleet1.insertAdjacentHTML('afterbegin', fleet);
+
+boardCells.forEach(boardCell => boardCell.addEventListener('click', checkMarkedCell));
